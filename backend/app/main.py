cf91c12b3236b89d94ee526d67be27178ea8c6e9
Finley <2746743726@qq.com>
2026-03-29 00:53:39 +0800

    chore: add Makefile in backend, and rewrite backend-unit-tests to test git action

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,15 @@
-'''
+"""
 Author: Finley
 创建时间: 2026-03-28 23:45:07
 最后修改时间: 2026-03-28 23:45:08
 文件路径: /SETSS_2026/backend/app/main.py
-文件描述: 
-'''
+文件描述:
+"""
+
 
 def main():
     print("Hello World!")
+
 
 if __name__ == "__main__":
     main()
--- backend/tests/test_tmp_main.py
+++ backend/tests/test_tmp_main.py
@@ -1,11 +1,11 @@
-'''
+"""
 Author: Finley
 创建时间: 2026-03-28 23:47:28
 最后修改时间: 2026-03-28 23:47:29
 文件路径: /SETSS_2026/backend/tests/test_tmp_main.py
 文件描述: 用来临时测试CI/CD流程的文件
-'''
+"""
+
 
 def test_main():
     assert True
-
